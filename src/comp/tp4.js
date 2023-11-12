19c454b3b99adc8896821579e955c66202a84964
nour-aassifar3 <nouraassifar10@gmail.com>
2023-11-12 23:41:52 +0100

    first commit

---
+++ src/comp/tp4.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./tp4.css";
+
+function Api() {
+  const [todos, setTodos] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
+      .then(response => setTodos(response.data))
+      .catch(error => console.error(error));
+  }, [page]);
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+      <button onClick={handlePreviousPage}>Précédent</button>
+      <button onClick={handleNextPage}>Suivant</button>
+    </div>
+  );
+}
+
+export default Api;
+
+
+
+
